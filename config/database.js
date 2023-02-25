c02ba5fe98418fce02b408d91718a0e39740193d
milind_sansare-17 <milind.sansare@ayekart.com>
2023-02-25 19:28:48 +0530

    gg

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDatabase = () => {
     // mongoose.set('strictQuery', false);
     mongoose.set('strictQuery', false)
-    mongoose.connect("mongodb://mydatabase.mongo.render.com:27017/Ecommrece",{
+    mongoose.connect("mongodb://127.0.0.1:27017/Ecommrece",{
       // mongodb://myusername:mypassword@mydatabase.mongo.render.com:27017/mydatabase
         useNewUrlParser: true,
         // useFindAndModify: false,
